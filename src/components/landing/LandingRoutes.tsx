d48febb3901087953a01a0b98a63ee8b7016e257
a-goaba <abdulrahman1997abdo@gmail.com>
2024-01-22 08:35:07 +0500

    add teachers component

--- src/components/landing/LandingRoutes.tsx
+++ src/components/landing/LandingRoutes.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 import Navbar from "./shared/Navbar"
 import Hero from './home/Hero';
 import About from './about/About';
+import Teachers from './teachers/TeachersList';
 
 const heroData = {
   title: 'Welcome to Al-Nahdah School',
@@ -11,12 +12,40 @@ const heroData = {
   videoFileName: '../../../public/landing/hero.mp4'
 };
 
+const teachersData = [
+  {
+    id: 1,
+    name: 'John Doe',
+    subject: 'Mathematics',
+    imageUrl: 'https://images.unsplash.com/photo-1568602471122-7832951cc4c5?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    subject: 'Science',
+    imageUrl: 'https://images.unsplash.com/photo-1557862921-37829c790f19?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    id: 1,
+    name: 'John Doe',
+    subject: 'Mathematics',
+    imageUrl: 'https://images.unsplash.com/photo-1564564321837-a57b7070ac4f?q=80&w=2076&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    id: 2,
+    name: 'Jane Smith',
+    subject: 'Science',
+    imageUrl: 'https://images.unsplash.com/photo-1539571696357-5a69c17a67c6?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+];
+
 const LandingRoutes = () => {
   return (
     <div >
       <Navbar />
       <Hero {...heroData} />
       <About />
+      <Teachers teachers={teachersData} />
     </div>
   );
 };
---
+++ src/components/landing/data/data.ts
@@ -0,0 +1,8 @@
+export const accordions = [
+  {
+    id: 1,
+    question: "What subjects do you offer?",
+    answer: "We offer a wide range of subjects, including but not limited to...",
+  },
+  // Add more accordion data as needed
+];
